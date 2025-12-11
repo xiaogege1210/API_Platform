57e42a1e20bd5b4bb0a8c7a1fca666f6902af7d8
wugeqi <2466300758@qq.com>
2025-12-11 12:45:58 +0800

    加入自动修复代码功能（生成代码不完整）

--- src/main/java/com/example/demo/service/TestCaseAnalysisService.java
+++ src/main/java/com/example/demo/service/TestCaseAnalysisService.java
@@ -1,8 +1,14 @@
 package com.example.demo.service;
+import com.example.demo.dto.TestCaseResultDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 
 public interface TestCaseAnalysisService {
     //拼接提示词，调用ai
     public String generateAnalysisAndSuggestions(String api, String environment, String dependency, String test, String testResult) throws JsonProcessingException;
+
+
+    String generateAnalysisAndSuggestions(String api, String test);
+
+    String OptimizedScript(String test, TestCaseResultDto testResult) throws JsonProcessingException;
 }
